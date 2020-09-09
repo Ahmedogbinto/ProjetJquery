8d1c56d6c4c1cdc135da2062aa3da2abd177a944
Ahmed OGBINTO <ahmedogbinto@gmail.com>
2020-09-09 14:59:43 -0500

    Finish with opned and closed step in Jquery

--- script.js
+++ script.js
@@ -2,23 +2,45 @@ $(function(){
     var $mainMenuItems = $("#main-menu ul").children("li"),
      totalMainMenuItems = $mainMenuItems.length,
      openedIndex = -1,
-     
+
      init = function(){
-
-         $mainMenuItems.children(".images").click(function(){
-
+         // opened function 1 //
+            $mainMenuItems.children(".images").click(function(){
              var newIndex = $(this).parent().index(),
-             $item = $mainMenuItems.eq(newIndex),
-             $colorImage = $item.find(".color");
+             $item = $mainMenuItems.eq(newIndex);
 
-             $colorImage.animate({left:"0px"},250);
-             $item.animate ({width: "420px"},250);
-
-             OpenedIndex = newIndex;
-
+                if( openedIndex === newIndex)
+                {
+                    animateItem($item, false, 250);
+                    openedIndex = -1;
+                }
+                else
+                {
+                    if (validIndex(newIndex) )
+                    {
+                        animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                        openedIndex = newIndex;
+                        animateItem($item, true, 250);
+                    }
+                                                                // here I use now my animate function create before
+                }
          });
-    };
+     },
 
+                                                                // creation a small verification fonction
+            validIndex = function(indexTocheck){
+                return (indexTocheck >=0 && indexTocheck <  totalMainMenuItems);
+                },
+
+                                                                // create parameter to used in init function
+            animateItem = function($item, toOpned, speed){
+            var $colorImage = $item.find(".color"),
+            itemParam = toOpned ? {width: "420px"}: {width: "140px"},
+            colorImageParam = toOpned ? {left: "0px"} : {left: "140px"}
+            $colorImage.animate(colorImageParam,speed);
+            $item.animate (itemParam,speed);
+     };
+    
     init();
 
 });
