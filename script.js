2e5f05e9ada0ddf8632e22aab646bb43ecc774ea
Ahmed OGBINTO <ahmedogbinto@gmail.com>
2020-09-10 18:39:13 -0500

    Jquery Reorganization  and add beginning animation

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(function(){
     var $mainMenuItems = $("#main-menu ul").children("li"),
      totalMainMenuItems = $mainMenuItems.length,
-     openedIndex = -1,
+     openedIndex = 2,
 
      init = function(){
-         // opened function 1 //
+        bindEnvent();
+                                                                    // 6°) Overture de l'item de l'index 2 au demarage
+        if (validIndex=(openedIndex)){
+            animateItem($mainMenuItems.eq(openedIndex), true, 700);
+         }
+     },
+        bindEnvent = function(){
+                                                                    // 1°) opened function 1 //
+                                                                    // 3°) here I use now my animate function create before
             $mainMenuItems.children(".images").click(function(){
              var newIndex = $(this).parent().index(),
              $item = $mainMenuItems.eq(newIndex);
@@ -16,23 +24,21 @@ $(function(){
                 }
                 else
                 {
-                    if (validIndex(newIndex) )
-                    {
+                    if (validIndex(newIndex) )                       // 5°) I use my indexTocheck parameter
+                    {                                                                
                         animateItem($mainMenuItems.eq(openedIndex), false, 250);
                         openedIndex = newIndex;
                         animateItem($item, true, 250);
-                    }
-                                                                // here I use now my animate function create before
+                    }                                          
                 }
          });
-     },
 
-                                                                // creation a small verification fonction
+        },
+                                                                    // 4°) creation a small verification fonction
             validIndex = function(indexTocheck){
                 return (indexTocheck >=0 && indexTocheck <  totalMainMenuItems);
                 },
-
-                                                                // create parameter to used in init function
+                                                                    // 2°) create parameter to used in init function
             animateItem = function($item, toOpned, speed){
             var $colorImage = $item.find(".color"),
             itemParam = toOpned ? {width: "420px"}: {width: "140px"},
