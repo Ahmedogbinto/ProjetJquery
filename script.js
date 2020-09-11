14b4070e0d229f1bd33fe9d0f9d7022ad0979c77
Ahmed OGBINTO <ahmedogbinto@gmail.com>
2020-09-11 05:03:03 -0500

    Button activation and Jquery code refactoring

--- script.js
+++ script.js
@@ -4,48 +4,68 @@ $(function(){
      openedIndex = 2,
 
      init = function(){
-        bindEnvent();
+        bindEvents();
                                                                     // 6°) Overture de l'item de l'index 2 au demarage
-        if (validIndex=(openedIndex)){
+        if (validIndex(openedIndex))
+         {
             animateItem($mainMenuItems.eq(openedIndex), true, 700);
          }
      },
-        bindEnvent = function(){
+             bindEvents = function(){
                                                                     // 1°) opened function 1 //
                                                                     // 3°) here I use now my animate function create before
-            $mainMenuItems.children(".images").click(function(){
-             var newIndex = $(this).parent().index(),
-             $item = $mainMenuItems.eq(newIndex);
+                $mainMenuItems.children(".images").click(function(){
+                var newIndex = $(this).parent().index();
+                    checkAndAnimateItem(newIndex);
+                });
 
-                if( openedIndex === newIndex)
-                {
-                    animateItem($item, false, 250);
-                    openedIndex = -1;
-                }
-                else
-                {
-                    if (validIndex(newIndex) )                       // 5°) I use my indexTocheck parameter
-                    {                                                                
-                        animateItem($mainMenuItems.eq(openedIndex), false, 250);
-                        openedIndex = newIndex;
-                        animateItem($item, true, 250);
-                    }                                          
-                }
-         });
+                                                                    // 7°) button activation
+                $(".button").hover(
+                    function(){
+                        $(this).addClass("hovered");
+                    },
+                    function(){
+                        $(this).removeClass("hovered");
+                    });
+                    
+                $(".button").click(function(){
+                    var newIndex = $(this).index();
+                    checkAndAnimateItem(newIndex);
 
-        },
+                });
+            },
                                                                     // 4°) creation a small verification fonction
-            validIndex = function(indexTocheck){
-                return (indexTocheck >=0 && indexTocheck <  totalMainMenuItems);
-                },
+            validIndex = function(indexToCheck){
+                return (indexToCheck >=0) && (indexToCheck <  totalMainMenuItems);
+            },
+
                                                                     // 2°) create parameter to used in init function
             animateItem = function($item, toOpned, speed){
-            var $colorImage = $item.find(".color"),
-            itemParam = toOpned ? {width: "420px"}: {width: "140px"},
-            colorImageParam = toOpned ? {left: "0px"} : {left: "140px"}
-            $colorImage.animate(colorImageParam,speed);
-            $item.animate (itemParam,speed);
-     };
+                var $colorImage = $item.find(".color"),
+                itemParam = toOpned ? {width: "420px"}: {width: "140px"},
+                colorImageParam = toOpned ? {left: "0px"} : {left: "140px"}
+                $colorImage.animate(colorImageParam,speed);
+                $item.animate (itemParam,speed);
+            };
+                                                                    // 8°) Recfactoring code repeat.... 
+                                                                    // 5°) I use my indexToCheck parameter)
+                checkAndAnimateItem = function(indexToCheckAndAnimate){
+                    if(openedIndex === indexToCheckAndAnimate)
+                    {
+                        animateItem($mainMenuItems, false, 250);
+                        openedIndex = -1;
+                    }
+                    else
+                    {
+                        if(validIndex(indexToCheckAndAnimate))                        // 5°) I use my indexToCheck parameter
+                        {                                                                
+                            animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                            openedIndex = indexToCheckAndAnimate;
+                            animateItem($mainMenuItems.eq(openedIndex), true, 250);
+                        }                                          
+                    }   
+
+                 };
     
     init();
 
